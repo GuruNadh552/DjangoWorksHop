650b1283f24fe1ddf6e1a5b7629a9ee77b000f55
GuruNadh552 <gurunadhpukkalla@gmail.com>
2020-07-19 10:36:35 +0530

    YesterDay Task Added

--- College/Faculty/urls.py
+++ College/Faculty/urls.py
@@ -2,5 +2,8 @@ from django.urls import path,include
 from Faculty import views
 
 urlpatterns = [
-   path('display/',views.display)
+   path('display/',views.display,name="display"),
+   path('FReg/',views.FReg,name="fregister"),
+   path('StudentReg/',views.SReg,name="sreg"),
+   path('table/<int:num>',views.table,name="table")
 ]
--- College/Faculty/views.py
+++ College/Faculty/views.py
@@ -10,3 +10,33 @@ def display(req):
 		"age" : "18"
 	}
 	return render(req,'Faculty/showdata.html',{'data':info})
+def FReg(req):
+	if (req.method=="POST"):
+		name = req.POST['name']
+		email = req.POST['email']
+		ID = req.POST['id']
+		age = req.POST['age']
+		info = {
+			'name' : name,
+			'email': email,
+			'ID' :ID,
+			'Age' : age
+		}
+		return render(req,'Faculty/success.html',{'data':info})
+	return render(req,'Faculty/FacultyReg.html')
+def SReg(req):
+	if req.method=="POST":
+		name = req.POST['name']
+		password = req.POST['password']
+		email = req.POST['email']
+		if (name == password):
+			return HttpResponse('<h1> Welcome ' + name + '</h1>')
+		else:
+			return HttpResponse('<h1>Invalid Credentials</h1>')
+	return render(req,'Faculty/StudentReg.html')
+
+def table(req,num):
+	x=[]
+	for i in range(1,11):
+		x.append(str(num) +' X ' + str(i) + " = " + str(num*i))
+	return render(req,'Faculty/table.html',{'data':x})
